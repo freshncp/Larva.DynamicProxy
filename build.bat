c1ec9a413cfb591f995ec6e9e202622f882b1466
Jerry Bai <chinaren_bjr@163.com>
2019-10-02 15:36:00 +0800

    nuget生成目录调整

--- build.bat
+++
@@ -1,9 +0,0 @@
-REM build
-dotnet build -c Release -f 'netstandard1.6' -o %~dp0nuget/lib/netstandard1.6/ %~dp0src/Larva.DynamicProxy/
-dotnet build -c Release -f 'netstandard2.0' -o %~dp0nuget/lib/netstandard2.0/ %~dp0src/Larva.DynamicProxy/
-
-REM test
-dotnet test %~dp0src/DynamicProxyTests/
-
-REM pack
-nuget pack -OutputDirectory %~dp0packages/ %~dp0nuget/Larva.DynamicProxy.nuspec
