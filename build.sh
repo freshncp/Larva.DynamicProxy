62cb2893852b071b90d6f3a578897f1a6a9a3bf4
Jerry Bai <chinaren_bjr@163.com>
2019-10-02 13:33:31 +0800

    build 增加 test脚本

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
+# build
 dotnet build -c Release -f 'netstandard1.6' -o `pwd`/nuget/lib/netstandard1.6/ `pwd`/src/Larva.DynamicProxy/
 dotnet build -c Release -f 'netstandard2.0' -o `pwd`/nuget/lib/netstandard2.0/ `pwd`/src/Larva.DynamicProxy/
+
+# test
+dotnet test `pwd`/src/DynamicProxyTests/
+
+# clean
 rm -rf nuget/.DS_Store
 rm -rf nuget/*/.DS_Store
 rm -rf nuget/*/*/.DS_Store
 
+# pack
 nuget pack -OutputDirectory `pwd`/packages/ `pwd`/nuget/Larva.DynamicProxy.nuspec
